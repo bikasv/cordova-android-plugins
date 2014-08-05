a236f014b4b04e36a74e24e8e061649a89c359c6
Jordan <jordan@designerwebspace.com>
2014-08-04 21:04:52 -0300

    Update DatePickerPlugin.java
    made the time picker respect the phone's format preference

--- datepicker/DatePickerPlugin.java
+++ datepicker/DatePickerPlugin.java
@@ -107,7 +107,7 @@ public class DatePickerPlugin extends CordovaPlugin {
                 public void run() {
                     final TimeSetListener timeSetListener = new TimeSetListener(datePickerPlugin, callbackContext);
                     final TimePickerDialog timeDialog = new TimePickerDialog(currentCtx, timeSetListener, mHour,
-                            mMinutes, true);
+                            mMinutes, DateFormat.is24HourFormat(currentCtx));
                     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
                         timeDialog.setCancelable(true);
                         timeDialog.setCanceledOnTouchOutside(false);
